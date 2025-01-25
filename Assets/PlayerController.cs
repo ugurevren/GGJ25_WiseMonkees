1316194b44228d072b186a784f563092dd29b655
Yankı Ufuk Dülger <65710392+YankUfuk@users.noreply.github.com>
2025-01-25 15:20:29 +0300

    Test

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,8 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public partial class PlayerController : MonoBehaviour
 {
+   // yapay zekaya yazdirdim yarin halletcez
+   private bool isGrounded;
+   private Rigidbody rb;
+   private float jumpForce = 5f;
+   // 31
    
+   //write jump script here
+   void Jump()
+   {
+      if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+      {
+         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+         isGrounded = false;
+      }
+   }
 }
