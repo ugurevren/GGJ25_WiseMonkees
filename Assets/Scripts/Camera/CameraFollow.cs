409faa5f749fe1f2ef76ea8fcc03c4ee70a22c74
Yankı Ufuk Dülger <65710392+YankUfuk@users.noreply.github.com>
2025-01-25 19:12:17 +0300

    Update PlayerController and CameraFollow

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -3,16 +3,44 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player; // Reference to the player's Transform
-    public Vector3 offset;   // Offset of the camera from the player
-    public float smoothSpeed = 0.125f; // How smooth the camera follows
+    public Transform targetLocation; // Reference to the assigned location Transform
+    private bool isPlayerOutOfBounds = false;
 
-    void FixedUpdate()
+    void LateUpdate()
     {
-        if (player != null)
+        if (isPlayerOutOfBounds)
         {
-            Vector3 desiredPosition = player.position + offset;
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-            transform.position = smoothedPosition;
+            MoveToTargetLocation();
+        }
+        else if (player != null)
+        {
+            if (IsPlayerOutOfScreen())
+            {
+                isPlayerOutOfBounds = true;
+            }
+            else
+            {
+                FollowPlayer();
+            }
+        }
+    }
+
+    void FollowPlayer()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+
+    bool IsPlayerOutOfScreen()
+    {
+        Vector3 playerScreenPosition = Camera.main.WorldToViewportPoint(player.position);
+        return playerScreenPosition.x < 0 || playerScreenPosition.x > 1 || playerScreenPosition.y < 0 || playerScreenPosition.y > 1;
+    }
+
+    void MoveToTargetLocation()
+    {
+        if (targetLocation != null)
+        {
+            transform.position = new Vector3(targetLocation.position.x, targetLocation.position.y, transform.position.z);
         }
     }
 }
