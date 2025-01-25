693ab29ee209b475587f0366c6820c9b759ee2fd
Yankı Ufuk Dülger <65710392+YankUfuk@users.noreply.github.com>
2025-01-25 19:27:40 +0300

    Update Camera

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,25 +4,54 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform player; // Reference to the player's Transform
     public Transform targetLocation; // Reference to the assigned location Transform
-    private bool isPlayerOutOfBounds = false;
+    public float moveSpeed = 5f; // Speed at which the camera moves to the target location
+    private bool isFollowingPlayer = true; // Flag to track if the camera is following the player
+    private bool isPlayerFalling = false; // Flag to track if the player is falling
+    private float previousPlayerY; // To store the previous Y position of the player
+
+    void Start()
+    {
+        if (player != null)
+        {
+            previousPlayerY = player.position.y;
+        }
+    }
 
     void LateUpdate()
     {
-        if (isPlayerOutOfBounds)
-        {
-            MoveToTargetLocation();
-        }
-        else if (player != null)
+        if (player == null) return;
+
+        if (isFollowingPlayer)
         {
             if (IsPlayerOutOfScreen())
             {
-                isPlayerOutOfBounds = true;
+                isFollowingPlayer = false;
             }
             else
             {
-                FollowPlayer();
+                if (isPlayerFalling)
+                {
+                    if (player.position.y > previousPlayerY)
+                    {
+                        isPlayerFalling = false;
+                    }
+                }
+                else
+                {
+                    if (player.position.y < previousPlayerY)
+                    {
+                        isPlayerFalling = true;
+                    }
+                    FollowPlayer();
+                }
             }
         }
+        else
+        {
+            MoveToTargetLocation();
+        }
+
+        previousPlayerY = player.position.y;
     }
 
     void FollowPlayer()
@@ -40,7 +69,7 @@ public class CameraFollow : MonoBehaviour
     {
         if (targetLocation != null)
         {
-            transform.position = new Vector3(targetLocation.position.x, targetLocation.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, new Vector3(targetLocation.position.x, targetLocation.position.y, transform.position.z), moveSpeed * Time.deltaTime);
         }
     }
 }
